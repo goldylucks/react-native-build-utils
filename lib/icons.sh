fa95fd3e3922b3df57bffd025d786bcaa6b93872
Adam Goldman <adamgoldman7@gmail.com>
2017-01-27 10:38:36 +0200

    (icons): change icon name

--- lib/icons.sh
+++ lib/icons.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e # stop on error
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
+cd ..
+cd ..
 cd ..
 
 type convert >/dev/null 2>&1 || { 
@@ -14,13 +15,13 @@ type convert >/dev/null 2>&1 || {
   exit 1
 }
 
-convert android/app/src/main/res/mipmap-mdpi/icon.png -shave 6x6 -bordercolor '#EF4142' -border 6 android/app/src/main/res/mipmap-mdpi/icon_dev.png
-convert android/app/src/main/res/mipmap-hdpi/icon.png -shave 8x8 -bordercolor '#EF4142' -border 8 android/app/src/main/res/mipmap-hdpi/icon_dev.png
-convert android/app/src/main/res/mipmap-xhdpi/icon.png -shave 10x10 -bordercolor '#EF4142' -border 10 android/app/src/main/res/mipmap-xhdpi/icon_dev.png
-convert android/app/src/main/res/mipmap-xxhdpi/icon.png -shave 12x12 -bordercolor '#EF4142' -border 12 android/app/src/main/res/mipmap-xxhdpi/icon_dev.png
+convert android/app/src/main/res/mipmap-mdpi/ic_launcher.png -shave 6x6 -bordercolor '#EF4142' -border 6 android/app/src/main/res/mipmap-mdpi/ic_launcher_dev.png
+convert android/app/src/main/res/mipmap-hdpi/ic_launcher.png -shave 8x8 -bordercolor '#EF4142' -border 8 android/app/src/main/res/mipmap-hdpi/ic_launcher_dev.png
+convert android/app/src/main/res/mipmap-xhdpi/ic_launcher.png -shave 10x10 -bordercolor '#EF4142' -border 10 android/app/src/main/res/mipmap-xhdpi/ic_launcher_dev.png
+convert android/app/src/main/res/mipmap-xxhdpi/ic_launcher.png -shave 12x12 -bordercolor '#EF4142' -border 12 android/app/src/main/res/mipmap-xxhdpi/ic_launcher_dev.png
 
-echo "All done! created icon_dev.png files in the following directories:"
-echo "android/app/src/main/res/mipmap-mdpi/icon_dev.png"
-echo "android/app/src/main/res/mipmap-hdpi/icon_dev.png"
-echo "android/app/src/main/res/mipmap-xhdpi/icon_dev.png"
-echo "android/app/src/main/res/mipmap-xxhdpi/icon_dev.png"
+echo "All done! created ic_launcher_dev.png files in the following directories:"
+echo "android/app/src/main/res/mipmap-mdpi/ic_launcher_dev.png"
+echo "android/app/src/main/res/mipmap-hdpi/ic_launcher_dev.png"
+echo "android/app/src/main/res/mipmap-xhdpi/ic_launcher_dev.png"
+echo "android/app/src/main/res/mipmap-xxhdpi/ic_launcher_dev.png"
